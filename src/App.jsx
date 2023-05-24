dd9f4e962c46984b64e2b90eb5c0d0f0f22f2bd2
Conrado Els <conradoels@gmail.com>
2023-05-23 21:27:44 -0300

    PreEntrega1-Els

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import './App.css'
-import NavBar from './components/layout/Header/NavBar/NavBar';
-import ItemListContainer from './components/pages/ItemListContainer/ItemListContainer';
+import NavBar from "./components/layout/Header/NavBar/NavBar";
+import ItemListContainer from "./components/pages/ItemListContainer/ItemListContainer";
 
 function App() {
-  
-
   return (
     <>
       <NavBar />
-      <ItemListContainer greeting={'Bienvenidos a "SWEET HOME" mi e-commerce de pastelería'}/>
+      <ItemListContainer
+        greeting={'Bienvenidos a "SWEET HOME" mi e-commerce de pastelería'}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/pages/ItemListContainer/ItemListContainer.jsx
+++ src/components/pages/ItemListContainer/ItemListContainer.jsx
@@ -3,11 +3,14 @@ const ItemListContainer = ({ greeting }) => {
   return (
     <>
       <div>
-        <h1 style={{textAlign: 'center', color: 'var(--sweet-home)'}}>{greeting}</h1>
+        <h1 style={{ textAlign: "center", color: "var(--sweet-home)" }}>
+          {greeting}
+        </h1>
         <img
           src="https://res.cloudinary.com/dtvlycezq/image/upload/v1684881641/Sweet%20Home/logo_size_invert_y9knht.jpg"
           alt="logo banner"
-        style={{width: '100%'}}/>
+          style={{ width: "100%" }}
+        />
       </div>
     </>
   );
