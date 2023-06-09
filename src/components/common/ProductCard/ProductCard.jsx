8ae1ab6d148bea3b75973b75fd2a4b72b4cf9d86
Conrado Els <conradoels@gmail.com>
2023-06-09 01:33:04 -0300

    modifico css de la card

--- src/components/common/ProductCard/ProductCard.jsx
+++ src/components/common/ProductCard/ProductCard.jsx
@@ -1,25 +1,30 @@
-import "./ProductCard.css"
-
-
+import "./ProductCard.css";
 
 const ProductCard = () => {
   return (
     <div className="cards-container">
-    <div className="product-card">
-            <img src="https://cdn.pixabay.com/photo/2015/05/29/19/18/bicycle-789648_1280.jpg" alt="product" className="product-img"/>
-            <div className="product-info">
-                <div>
-                    <p>Price</p>
-                    <p>Name</p>
-                    <p>Description</p>
-                </div>
-                <figure>
-                <img src="/public/assets/bt_add_to_cart.svg" alt="cart" />
-                </figure>
-            </div>
+      <div className="product-card">
+        <img
+          src="https://res.cloudinary.com/dtvlycezq/image/upload/v1686244696/Sweet%20Home/Productos/Tortas%20y%20Postres/BOMB%C3%93N_pkhf45.jpg"
+          alt="product"
+          className="product-img"
+        />
+        <div className="product-info">
+          <div>
+            <p>$6700</p>
+            <p>BOMBÓN</p>
+            <p>
+              Torta de chocolate rellena de dulce de leche con chocolate y
+              coñac.
+            </p>
+          </div>
+          <figure>
+            <img src="/public/assets/bt_add_to_cart.svg" alt="cart" />
+          </figure>
         </div>
-        </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
