1afe1f3f1b47a5f735ddb5603ef3083d14d60a4b
Conrado Els <conradoels@gmail.com>
2023-06-12 15:08:16 -0300

    estilos en product card

--- src/components/common/ProductCard/ProductCard.jsx
+++ src/components/common/ProductCard/ProductCard.jsx
@@ -5,7 +5,7 @@ const ProductCard = ({ element }) => {
   return (
       <div className="product-card">
         <img src={element.img} alt="product" className="product-img" />
-        <div className="product-info">
+        <div className="product-infor">
           <div>
             <p>${element.price}</p>
             <p>{element.title}</p>
