79d250e6c2330b14a8f8cc2123a4c9e909eb219d
Conrado Els <conradoels@gmail.com>
2023-07-13 12:35:59 -0300

    footer

---
+++ src/components/layout/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div>
+        <img
+          src="https://res.cloudinary.com/dtvlycezq/image/upload/v1684851983/Sweet%20Home/white_logo_color_background_tcersk.jpg"
+          alt="logo"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "./Header/NavBar/NavBar"
+import Footer from "./Footer/Footer"
 
 
 const Layout = () => {
@@ -7,6 +8,7 @@ const Layout = () => {
     <div>
         <NavBar /> 
         <Outlet />
+        <Footer />
     </div>
   )
 }
