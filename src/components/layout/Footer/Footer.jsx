45cfe0b8df1ac4793edad68f5af9a2b1947406e8
Conrado Els <conradoels@gmail.com>
2023-07-14 20:24:20 -0300

    edito logo del footer

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -3,12 +3,10 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <div>
-        <img
-          src="https://res.cloudinary.com/dtvlycezq/image/upload/v1684851983/Sweet%20Home/white_logo_color_background_tcersk.jpg"
-          alt="logo"
-        />
-      </div>
+      <img
+        src="https://res.cloudinary.com/dtvlycezq/image/upload/v1684851983/Sweet%20Home/white_logo_color_background_tcersk.jpg"
+        alt="logo"
+      />
     </div>
   );
 };
