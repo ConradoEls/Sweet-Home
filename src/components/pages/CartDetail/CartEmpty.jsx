aaae58c4c76fdf0811e1492653ec2a4fdaeb9354
Conrado Els <conradoels@gmail.com>
2023-07-16 23:17:29 -0300

    responsive CartEmpty

--- src/components/pages/CartDetail/CartEmpty.jsx
+++ src/components/pages/CartDetail/CartEmpty.jsx
@@ -5,13 +5,13 @@ const CartEmpty = () => {
   return (
     <div className="cart-empty">
       <h2>El carrito esta vacio</h2>
-      <Link to={"/"} className="primary-button"> 
+      <Link to={"/"} className="back-shop"> 
       <button className="shop-button">
       COMPRAR
       </button>
       </Link>
       
-      <img
+      <img className="info-sweet"
         src="https://res.cloudinary.com/dtvlycezq/image/upload/v1684852092/Sweet%20Home/Dorso_1_rbhnd0.jpg"
         alt=""
       />
